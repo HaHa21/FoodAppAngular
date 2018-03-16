f04da05d3f9b5a153237895b4700d8a281f6b2da
HaHa21 <domlee2012@gmail.com>
2018-03-16 20:57:09 +0000

    Committing Sub-Components and Mainpages that are completed. Routing is also done.

---
+++ src/app/Components/app-header/app-header.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component, Input } from '@angular/core';
+import { AppHeader } from './app-header.model';
+
+@Component({
+  selector: 'app-app-header',
+  templateUrl: './app-header.component.html',
+  styleUrls: ['./app-header.component.css']
+})
+export class AppHeaderComponent {
+   @Input() appheader : AppHeader;
+  
+}
---
+++ src/app/Components/app-header/app-header.model.ts
@@ -0,0 +1,13 @@
+﻿export class AppHeader{
+   title : string;
+   foodlink : string;
+   drinklink : string;
+   aboutus : string;
+
+   constructor(title : string, foodlink : string, drinklink : string, aboutus : string){
+      this.title = title;
+      this.foodlink = foodlink;
+      this.drinklink = drinklink;
+      this.aboutus = aboutus;
+   }
+}
