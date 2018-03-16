5e40d480004a205928046890bf231d0c34de9091
HaHa21 <domlee2012@gmail.com>
2018-03-16 21:05:28 +0000

    Updated app.module and app.component. These are the backbone.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { AppHeader } from './Components/app-header/app-header.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  appheader : AppHeader = new AppHeader ("Ben Fast Food", "Food", "Drinks", "AboutUs");
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './Components/app-header/app-header.component';
+import { AppFooterComponent } from './Components/app-footer/app-footer.component';
+import { AppBodyComponent } from './Components/app-body/app-body.component';
+import { AboutUsComponent } from './Mainpages/about-us/about-us.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './Mainpages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppBodyComponent,
+    AboutUsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
